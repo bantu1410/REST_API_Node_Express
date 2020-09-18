8074e357489e509e0570b0c7cd3c30d25500f324
Naveen Bantu <naveen.bantu1@gmail.com>
2020-09-18 13:44:02 +0200

    Added MongodB connection and Querying Project infos from the database

---
+++ models/projModel.js
@@ -0,0 +1,12 @@
+/* eslint linebreak-style: ["error", "windows"] */
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const projModel = new Schema(
+  {
+    sytemName: { type: String },
+  },
+);
+
+module.exports = mongoose.model('Project', projModel, 'SysInfo');
